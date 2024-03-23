edf69523454ff632a098c3cbf00b80e0d43ca7e3
3uclid3 <3uclid3@pm.me>
2024-03-23 11:32:49 -0400

    Add support for custom style variables in ImGui wrapper. Introduces a struct to manage ImGui style variable scopes.

--- lib/nesesan/nesesan/imgui/core.hpp
+++ lib/nesesan/nesesan/imgui/core.hpp
@@ -2,8 +2,34 @@
 
 #include <imgui.h>
 
+#include <nese/basic_types.hpp>
+
 namespace nese::san::imgui {
 
 using namespace ImGui;
+
+using vec2 = ImVec2;
+
+struct style_var_scope
+{
+    style_var_scope(int style_var, f32_t value);
+    style_var_scope(int style_var, vec2 value);
+    ~style_var_scope();
+};
+
+inline style_var_scope::style_var_scope(int style_var, f32_t value)
+{
+    PushStyleVar(style_var, value);
+}
+
+inline style_var_scope::style_var_scope(int style_var, vec2 value)
+{
+    PushStyleVar(style_var, value);
+}
+
+inline style_var_scope::~style_var_scope()
+{
+    PopStyleVar();
+}
 
 }
