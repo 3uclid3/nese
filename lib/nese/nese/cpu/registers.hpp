17b90aa78b4dca4dd632a747caab4aaa27b15011
3uclid3 <3uclid3@pm.me>
2024-03-24 17:07:19 -0400

    Refactor register struct with default values and new ALU flag setting method.
    - Added default values for registers
    - Implemented has_flag() method
    - Refactored set_flag() to modify status register 'p'
    - Introduced set_alu_flag() to update ALU flags

--- lib/nese/nese/cpu/registers.hpp
+++ lib/nese/nese/cpu/registers.hpp
@@ -5,21 +5,48 @@
 
 namespace nese::cpu {
 
+// A: Accumulator - supports using status register for carrying, overflow detection, etc
+// X: Index register. Used for addressing modes, loop counters
+// Y: Same as X
+// PC: Program counter. Supports 65535 direct (unbanked) memory locations.
+// S: Stack pointer. Can be accessed using interrupts, pulls, pushes, and transfers.
+// P: Status register - used by ALU unit
 struct registers
 {
+    static constexpr u8_t default_a{0};
+    static constexpr u8_t default_x{0};
+    static constexpr u8_t default_y{0};
+    static constexpr u16_t default_pc{0};
+    static constexpr u8_t default_s{0xfd};
+    static constexpr u8_t default_p{0x24};
+
+    [[nodiscard]] constexpr bool has_flag(status_flag flag) const;
     constexpr void set_flag(status_flag flag, bool value);
 
-    u8_t a{0};   // Accumulator - supports using status register for carrying, overflow detection, etc
-    u8_t x{0};   // Index register. Used for addressing modes, loop counters
-    u8_t y{0};   // Same as X
-    u16_t pc{0}; // Program counter. Supports 65535 direct (unbanked) memory locations.
-    u8_t s{0};   // Stack pointer. Can be accessed using interrupts, pulls, pushes, and transfers.
-    u8_t p{0};   // Status register - used by ALU unit
+    constexpr void set_alu_flag(byte_t value);
+
+    u8_t a{default_a};
+    u8_t x{default_x};
+    u8_t y{default_y};
+    u16_t pc{default_pc};
+    u8_t s{default_s};
+    u8_t p{default_p};
 };
+
+constexpr bool registers::has_flag(status_flag flag) const
+{
+    return (p & flag) != 0;
+}
 
 constexpr void registers::set_flag(status_flag flag, bool value)
 {
-    s = value ? s | flag : s & ~flag;
+    p = value ? p | flag : p & ~flag;
+}
+
+constexpr void registers::set_alu_flag(byte_t value)
+{
+    set_flag(status_flag::zero, value == 0);
+    set_flag(status_flag::negative, value & 0x80);
 }
 
 } // namespace nese::cpu
