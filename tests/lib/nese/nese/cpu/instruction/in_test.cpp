442a16d59a2db3b7fc9b497421f7e538d0d92969
3uclid3 <3uclid3@pm.me>
2024-03-29 14:59:40 -0400

    Refactor test_implied function parameters for INX and INY instructions
    Changed the test_implied function in the code to accept a register_id parameter instead of a SetRegisterFunctorT parameter for the INX and INY instructions. This simplifies the function signature and improves readability.

--- tests/lib/nese/nese/cpu/instruction/in_test.cpp
+++ tests/lib/nese/nese/cpu/instruction/in_test.cpp
@@ -8,8 +8,8 @@ namespace nese::cpu::instruction {
 
 struct in_fixture : fixture
 {
-    template<typename InstructionExecuteT, typename SetRegisterFunctorT>
-    void test_implied(const InstructionExecuteT& execute, const SetRegisterFunctorT& set_register)
+    template<typename InstructionExecuteT>
+    void test_implied(const InstructionExecuteT& execute, register_id increment_register)
     {
         SECTION("implied")
         {
@@ -17,13 +17,13 @@ struct in_fixture : fixture
 
             SECTION("increment 0xFF results in overflow to 0x00")
             {
-                set_register(state.registers, 0xFF);
+                set_register(state, increment_register , 0xFF);
 
                 expected_state = state;
                 expected_state.cycle = cycle_cost;
-                set_register(expected_state.registers, 0x00);
                 expected_state.registers.set_flag(status_flag::zero, true);
                 expected_state.registers.set_flag(status_flag::negative, false);
+                set_register(expected_state, increment_register, 0x00);
 
                 execute(state);
 
@@ -32,13 +32,13 @@ struct in_fixture : fixture
 
             SECTION("increment 0x7F results in overflow to 0x80")
             {
-                set_register(state.registers, 0x7F);
+                set_register(state, increment_register, 0x7F);
 
                 expected_state = state;
                 expected_state.cycle = cycle_cost;
-                set_register(expected_state.registers, 0x80);
                 expected_state.registers.set_flag(status_flag::zero, false);
                 expected_state.registers.set_flag(status_flag::negative, true);
+                set_register(expected_state, increment_register, 0x80);
 
                 execute(state);
 
@@ -47,17 +47,14 @@ struct in_fixture : fixture
 
             SECTION("increment negative")
             {
-                const byte_t value = GENERATE(byte_t{0x80},
-                                              byte_t{0xC0},
-                                              byte_t{0xFE});
-
-                set_register(state.registers, value);
+                const byte_t value = GENERATE(as<byte_t>(), 0x80, 0xC0, 0xFE);
+                set_register(state, increment_register, value);
 
                 expected_state = state;
                 expected_state.cycle = cycle_cost;
-                set_register(expected_state.registers, value + 1);
                 expected_state.registers.set_flag(status_flag::zero, false);
                 expected_state.registers.set_flag(status_flag::negative, true);
+                set_register(expected_state, increment_register, value + 1);
 
                 execute(state);
 
@@ -66,18 +63,14 @@ struct in_fixture : fixture
 
             SECTION("increment positive")
             {
-                const byte_t value = GENERATE(byte_t{0x00},
-                                              byte_t{0x10},
-                                              byte_t{0x40},
-                                              byte_t{0x7E});
-
-                set_register(state.registers, value);
+                const byte_t value = GENERATE(as<byte_t>(), 0x00, 0x10, 0x40, 0x7E);
+                set_register(state, increment_register, value);
 
                 expected_state = state;
                 expected_state.cycle = cycle_cost;
-                set_register(expected_state.registers, value + 1);
                 expected_state.registers.set_flag(status_flag::zero, false);
                 expected_state.registers.set_flag(status_flag::negative, false);
+                set_register(expected_state, increment_register, value + 1);
 
                 execute(state);
 
@@ -89,12 +82,12 @@ struct in_fixture : fixture
 
 TEST_CASE_METHOD(in_fixture, "inx", "[cpu][instruction]")
 {
-    test_implied(execute_inx<addr_mode::implied>, set_register_x);
+    test_implied(execute_inx<addr_mode::implied>, register_id::x);
 }
 
 TEST_CASE_METHOD(in_fixture, "iny", "[cpu][instruction]")
 {
-    test_implied(execute_iny<addr_mode::implied>, set_register_y);
+    test_implied(execute_iny<addr_mode::implied>, register_id::y);
 }
 
 } // namespace nese::cpu::instruction
