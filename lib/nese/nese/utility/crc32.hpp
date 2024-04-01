901206560adb5da2f666d541916b6c89dfcb22e9
3uclid3 <3uclid3@pm.me>
2024-04-01 18:07:23 -0400

    Update CRC32 calculation to use std::accumulate for efficiency.
    The commit refactors the CRC32 calculation function to utilize std::accumulate for improved efficiency and readability.

--- lib/nese/nese/utility/crc32.hpp
+++ lib/nese/nese/utility/crc32.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <numeric>
+
 #include <nese/basic_types.hpp>
 #include <nese/utility/details/crc32_table.hpp>
 
@@ -7,13 +9,9 @@ namespace nese {
 
 constexpr u32_t crc32(string_view string, u32_t crc = 0u)
 {
-    crc = crc ^ 0xFFFFFFFFu;
-    for (const char c : string)
-    {
-        crc = details::crc32_table[static_cast<u32_t>(c) ^ (crc & 0xFF)] ^ (crc >> 8);
-    }
-    crc = crc ^ 0xFFFFFFFFu;
-    return crc;
+    return std::accumulate(string.begin(), string.end(), crc, [](uint32_t current_crc, char c) {
+               return details::crc32_table[static_cast<uint8_t>(c) ^ (current_crc & 0xFF)] ^ (current_crc >> 8);
+           }) ^ 0xFFFFFFFFu;
 }
 
 } // namespace nese
