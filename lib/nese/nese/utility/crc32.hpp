ab97b7e1c45a2d2faba031e8b371353670fc6eaa
3uclid3 <3uclid3@pm.me>
2024-04-01 20:27:40 -0400

    Undid std::accumulator
    Breaks crc32

--- lib/nese/nese/utility/crc32.hpp
+++ lib/nese/nese/utility/crc32.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <numeric>
-
 #include <nese/basic_types.hpp>
 #include <nese/utility/details/crc32_table.hpp>
 
@@ -9,9 +7,13 @@ namespace nese {
 
 constexpr u32_t crc32(string_view string, u32_t crc = 0u)
 {
-    return std::accumulate(string.begin(), string.end(), crc, [](uint32_t current_crc, char c) {
-               return details::crc32_table[static_cast<uint8_t>(c) ^ (current_crc & 0xFF)] ^ (current_crc >> 8);
-           }) ^ 0xFFFFFFFFu;
+    crc = crc ^ 0xFFFFFFFFu;
+    for (const char c : string)
+    {
+        crc = details::crc32_table[static_cast<u32_t>(c) ^ (crc & 0xFF)] ^ (crc >> 8);
+    }
+    crc = crc ^ 0xFFFFFFFFu;
+    return crc;
 }
 
 } // namespace nese
