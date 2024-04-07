651b896326e6cb0ad1376389d5c70c7763da02ed
3uclid3 <3uclid3@pm.me>
2024-04-07 18:27:51 -0400

    Added missing implied SECTION

--- tests/lib/nese/nese/cpu/instruction/fixture/increment_fixture.cpp
+++ tests/lib/nese/nese/cpu/instruction/fixture/increment_fixture.cpp
@@ -10,56 +10,59 @@ namespace nese::cpu::instruction {
 
 void increment_fixture::test_implied(opcode opcode, register_id increment_register)
 {
-    constexpr cycle_t cycle_cost = cpu_cycle_t(2);
-
-    SECTION("increment 0xFF results in overflow to 0x00")
+    SECTION("implied")
     {
-        set_register(state().registers, increment_register, 0xFF);
+        constexpr cycle_t cycle_cost = cpu_cycle_t(2);
 
-        expected_state().cycle = cycle_cost;
-        expected_state().registers.set_flag(status_flag::zero, true);
-        expected_state().registers.set_flag(status_flag::negative, false);
-        set_register(expected_state().registers, increment_register, 0x00);
+        SECTION("increment 0xFF results in overflow to 0x00")
+        {
+            set_register(state().registers, increment_register, 0xFF);
 
-        execute_and_check(opcode);
-    }
+            expected_state().cycle = cycle_cost;
+            expected_state().registers.set_flag(status_flag::zero, true);
+            expected_state().registers.set_flag(status_flag::negative, false);
+            set_register(expected_state().registers, increment_register, 0x00);
 
-    SECTION("increment 0x7F results in overflow to 0x80")
-    {
-        set_register(state().registers, increment_register, 0x7F);
+            execute_and_check(opcode);
+        }
 
-        expected_state().cycle = cycle_cost;
-        expected_state().registers.set_flag(status_flag::zero, false);
-        expected_state().registers.set_flag(status_flag::negative, true);
-        set_register(expected_state().registers, increment_register, 0x80);
+        SECTION("increment 0x7F results in overflow to 0x80")
+        {
+            set_register(state().registers, increment_register, 0x7F);
 
-        execute_and_check(opcode);
-    }
+            expected_state().cycle = cycle_cost;
+            expected_state().registers.set_flag(status_flag::zero, false);
+            expected_state().registers.set_flag(status_flag::negative, true);
+            set_register(expected_state().registers, increment_register, 0x80);
 
-    SECTION("increment negative")
-    {
-        const byte_t value = GENERATE(as<byte_t>(), 0x80, 0xC0, 0xFE);
-        set_register(state().registers, increment_register, value);
+            execute_and_check(opcode);
+        }
 
-        expected_state().cycle = cycle_cost;
-        expected_state().registers.set_flag(status_flag::zero, false);
-        expected_state().registers.set_flag(status_flag::negative, true);
-        set_register(expected_state().registers, increment_register, value + 1);
+        SECTION("increment negative")
+        {
+            const byte_t value = GENERATE(as<byte_t>(), 0x80, 0xC0, 0xFE);
+            set_register(state().registers, increment_register, value);
 
-        execute_and_check(opcode);
-    }
+            expected_state().cycle = cycle_cost;
+            expected_state().registers.set_flag(status_flag::zero, false);
+            expected_state().registers.set_flag(status_flag::negative, true);
+            set_register(expected_state().registers, increment_register, value + 1);
 
-    SECTION("increment positive")
-    {
-        const byte_t value = GENERATE(as<byte_t>(), 0x00, 0x10, 0x40, 0x7E);
-        set_register(state().registers, increment_register, value);
+            execute_and_check(opcode);
+        }
 
-        expected_state().cycle = cycle_cost;
-        expected_state().registers.set_flag(status_flag::zero, false);
-        expected_state().registers.set_flag(status_flag::negative, false);
-        set_register(expected_state().registers, increment_register, value + 1);
+        SECTION("increment positive")
+        {
+            const byte_t value = GENERATE(as<byte_t>(), 0x00, 0x10, 0x40, 0x7E);
+            set_register(state().registers, increment_register, value);
 
-        execute_and_check(opcode);
+            expected_state().cycle = cycle_cost;
+            expected_state().registers.set_flag(status_flag::zero, false);
+            expected_state().registers.set_flag(status_flag::negative, false);
+            set_register(expected_state().registers, increment_register, value + 1);
+
+            execute_and_check(opcode);
+        }
     }
 }
 
